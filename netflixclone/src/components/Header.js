aa9f2e2b5bf6a4ed527280082c1954404eb03289
bucard <rafaelbucard@gmail.com>
2020-12-22 22:45:51 -0300

    icon netflix url

--- netflixclone/src/components/Header.js
+++ netflixclone/src/components/Header.js
@@ -6,7 +6,7 @@ export default ({black}) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix" />
+                    <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" alt="Netflix" />
                 </a>
             </div>
             <div className="header--user">
